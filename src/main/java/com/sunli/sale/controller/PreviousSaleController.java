a9900ea539be6dad36fba17a354b8a0cc2d0c4db
lh <18081139916@163.com>
2019-06-11 23:27:30 +0800

    test

--- src/main/java/com/sunli/sale/controller/PreviousSaleController.java
+++ src/main/java/com/sunli/sale/controller/PreviousSaleController.java
@@ -6,6 +6,8 @@ import com.sunli.sale.dto.reqDTO.PreviousSaleSelectListDTO;
 import com.sunli.sale.service.PreviousSaleService;
 import com.sunli.sale.utils.resultUtil.ResultMsg;
 import com.sunli.sale.utils.resultUtil.ResultUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -13,14 +15,17 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/rest/previous/sale")
+@Api(value = "销售业绩相关Controller", description = "销售业绩", tags = { "sale" })
 public class PreviousSaleController {
     @Resource
     private PreviousSaleService previousSaleService;
     @PostMapping("/insert/list")
+
     public ResultMsg insertList(@RequestBody PreviousSale previousSale){
         return ResultUtil.success(previousSaleService.insert(previousSale));
     }
     @PostMapping("/select/list")
+    @ApiOperation("查询销售业绩")
     public ResultMsg selectList(@RequestBody PreviousSaleSelectListDTO dto){
         PageInfo<PreviousSale> previousSalePageInfo = previousSaleService.selectList(dto);
         return ResultUtil.success(previousSalePageInfo);
